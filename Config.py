852b3826c9c498113811f07838e7958d3c68a75c
Zachary Mangiafesto <zmanja42@gmail.com>
2026-02-20 06:44:49 -0500

    Add configuration settings in Config.py

---
+++ Config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-change-me"
+    DATABASE = os.path.join(BASE_DIR, "instance", "timecard.db")
+    DEBUG = True
+
